139da5c11c45017ae4546b538a0e07090246f33c
PerrinoMichele <micheleperrino97@gmail.com>
2018-12-13 10:41:01 +0100

    New assets

---
+++ Pick and drop/Assets/scripts/PlayerLife2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLife2 : MonoBehaviour
+
+{
+
+    public static int Health;
+    // Use this for initialization
+    void Start()
+    {
+        Health = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Health == 0)
+        {
+            gameObject.SetActive(false);
+            Debug.Log("metoo");
+        }
+    }
+}
---
+++ Pick and drop/Assets/scripts/TNT.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TNT : MonoBehaviour
+{
+
+    public GameObject Explosion;
+    public bool Triggered = false;
+    public bool DestroyableObject1Detected = false;
+    public bool DestroyableObject2Detected = false;
+    public Transform player1;
+    public Transform player2;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag.Equals("Player1") || col.gameObject.tag.Equals("Player2"))
+        {
+            Triggered = true;
+            StartCoroutine (Destroy());
+            //start destroy anim
+        }
+    }
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag.Equals("Player1"))
+        {
+            DestroyableObject1Detected = true;
+            
+        }
+        else if (col.gameObject.tag.Equals("Player2"))
+        {
+            DestroyableObject2Detected = true;
+
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag.Equals("Player1"))
+        {
+            DestroyableObject1Detected = false;
+
+        }
+        else if(col.gameObject.tag.Equals("Player2"))
+        {
+            DestroyableObject2Detected = false;
+
+        }
+    }
+
+
+
+    IEnumerator Destroy()
+    {
+        yield return new WaitForSeconds(2f);
+       if(Triggered == true)
+        {
+            gameObject.SetActive(false);
+            Instantiate(Explosion, transform.position, Quaternion.identity);
+            //Sound
+        }
+
+        else if (Triggered == true && DestroyableObject1Detected == true)
+        {
+            gameObject.SetActive(false);
+            PlayerLife1.Health -= 1;
+        }
+
+        else if (Triggered == true && DestroyableObject2Detected == true)
+        {
+            gameObject.SetActive(false);
+            PlayerLife2.Health -= 1;     
+        }
+
+        else if (Triggered == true && DestroyableObject2Detected == true && DestroyableObject1Detected == true)
+        {
+            gameObject.SetActive(false);
+            PlayerLife2.Health -= 1;
+            PlayerLife1.Health -= 1;
+        }
+    }
+}
