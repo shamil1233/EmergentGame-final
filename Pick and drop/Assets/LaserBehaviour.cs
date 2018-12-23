fcd3974457fc0d8f2317cde768033c357f5c6daa
PerrinoMichele <micheleperrino97@gmail.com>
2018-12-23 13:13:16 +0100

    laser testing

---
+++ Pick and drop/Assets/LaserBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBehaviour : MonoBehaviour {
+    public float moveSpeed;
+
+    private Rigidbody2D myRigidbody;
+
+    // Use this for initialization
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
+    }
+}
