10054be57291aee668687e21d7fd93bc5d72a69f
shamil1233 <43807064+shamil1233@users.noreply.github.com>
2018-12-28 12:02:01 +0100

    up

---
+++ Pick and drop/Assets/scripts/PointEffector.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointEffector : MonoBehaviour {
+
+	public GameObject explosion;
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.B))
+		{
+			Instantiate(explosion, transform.position, Quaternion.identity);
+			gameObject.SetActive(false);
+
+			
+		}
+	}
+}
